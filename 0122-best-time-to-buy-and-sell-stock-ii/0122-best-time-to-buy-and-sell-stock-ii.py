0b3fe1740b2133424ed53eb1c280e2c26368ca96
Mayank Negi <itsmemayank02@gmail.com>
2026-06-02 23:41:47 +0530

    Time: 2 ms (67.12%), Space: 20.4 MB (51.11%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.py
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.py
@@ -1,15 +1,22 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        prof = 0 
-        maxprof = 0
-        mini = prices[0]
-        n = len(prices)
-        for i in range(n):
-            mini = min(mini, prices[i])
-            diff = prices[i] - mini 
-            prof = max(prof , diff)
-            if prof > 0:
-                maxprof +=prof
-                mini = prices[i]
-                prof = 0 
+        # prof = 0 
+        # maxprof = 0
+        # mini = prices[0]
+        # n = len(prices)
+        # for i in range(n):
+        #     mini = min(mini, prices[i])
+        #     diff = prices[i] - mini 
+        #     prof = max(prof , diff)
+        #     if prof > 0:
+        #         maxprof +=prof
+        #         mini = prices[i]
+        #         prof = 0 
+        # return maxprof
+
+        maxprof = 0 
+        for i in range(1, len(prices)):
+            if prices[i] > prices[i-1]:
+                maxprof+= prices[i] - prices[i-1]
+
         return maxprof
